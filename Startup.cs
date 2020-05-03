e3e33cc91ae3b697401a8deae4b0fcd5a6155ee7
Teng Wei Song <weisong.teng.work@gmail.com>
2020-05-04 00:09:41 +0800

    disabled https

--- Startup.cs
+++ Startup.cs
@@ -39,10 +39,10 @@ namespace CoffeeShopTalk
             {
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                // app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
