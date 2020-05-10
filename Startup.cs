d162def394f213a424fa2e006cc5f0f272b41abb
Teng Wei Song <weisong.teng.work@gmail.com>
2020-05-10 21:06:15 +0800

    updated forward header

--- Startup.cs
+++ Startup.cs
@@ -83,15 +83,23 @@ namespace CoffeeShopTalk
                             }
                         };
                     });
+            services.Configure<ForwardedHeadersOptions>(options =>
+            {
+                options.ForwardedHeaders =
+                    ForwardedHeaders.XForwardedFor |
+                    ForwardedHeaders.XForwardedHost |
+                    ForwardedHeaders.XForwardedProto;
+
+                options.ForwardLimit = 2;  //Limit number of proxy hops trusted
+                options.KnownNetworks.Clear();
+                options.KnownProxies.Clear();
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseForwardedHeaders(new ForwardedHeadersOptions()
-            {
-                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedHost | ForwardedHeaders.XForwardedProto
-            });
+            app.UseForwardedHeaders();
 
             if (env.IsDevelopment())
             {
